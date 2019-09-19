c4f8628bb5c88bbb1f493de46e4d06c8bcacf834
Marc-Andre Methot <marcandremethot@gmail.com>
2019-09-19 10:35:50 -0400

    Set up for SPA

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-require('./bootstrap');
+import './bootstrap';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+
+// Route information for Vue Router
+import Routes from '@/js/routes.js';
+
+// Component File
+import App from '@/js/views/App';
+
+Vue.use(Vuetify);
+
+const app = new Vue({
+	el: '#app',
+	router: Routes,
+	render: h => h(App),
+});
+
+export default app;
